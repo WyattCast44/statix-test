cc2c35e3b9e279bf5ee2e1f4e66ad9756476daeb
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2022-02-21 19:13:31 -0800

    wip

--- statix.php
+++ statix.php
@@ -1,5 +1,9 @@
 <?php
 
+echo 'Vendor exists: ' . is_dir('vendor') . PHP_EOL;
+echo 'Autoload exists: ' . file_exists('vendor/autoload.php') . PHP_EOL;
+die();
+
 use Statix\Application;
 
 require_once 'vendor/autoload.php';
