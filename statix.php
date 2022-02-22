c3584b3f4b250b48fddfdaf6ad0a27ddfb5dd193
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2022-02-21 19:16:57 -0800

    wip

--- statix.php
+++ statix.php
@@ -1,7 +1,7 @@
 <?php
 
 echo 'Vendor exists: ' . is_dir('vendor') . PHP_EOL;
-echo 'Autoload exists: ' . file_exists('vendor/autoload.php') . PHP_EOL;
+echo 'Autoload exists: ' . (int) file_exists('vendor/autoload.php') . PHP_EOL;
 die();
 
 use Statix\Application;
