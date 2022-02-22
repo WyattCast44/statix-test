f9203685a3660c0eae9be48508f04c001435d03e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2022-02-21 19:26:37 -0800

    wip

--- statix.php
+++ statix.php
@@ -2,6 +2,7 @@
 
 echo 'Vendor exists: ' . is_dir('vendor') . PHP_EOL;
 echo 'Autoload exists: ' . (int) file_exists('vendor/autoload.php') . PHP_EOL;
+echo 'PCNTL installed: ' . (int) extension_loaded('pcntl') . PHP_EOL;
 var_dump(scandir(__DIR__ . '/vendor'));
 die();
 
